db329c72bd07314af05e87192cb9440024fbd284
Telman Agababov <telman.agababov@gmail.com>
2017-11-05 19:55:22 +0200

    add todos

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,20 @@
 // TO DO
 angular.module('angular', [])
-.controller('CustomCtrl', function() {
-    this.todoname = 'test';
-});
+    .controller('CustomCtrl', function () {
+        var self = this;
+
+        self.todoPlaceholder = 'enter task';
+        self.todoname = '';
+        self.todos = [
+            'learn angular',
+            'learn react',
+            'learn meteor'
+        ];
+
+        self.addTodo = function () {
+            if (self.todoname !== '') {
+                self.todos.push(self.todoname);
+                self.todoname = '';
+            }
+        }
+    });
